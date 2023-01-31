61d674f3507b325ec4781ea59016630200edf784
Mustafa Eideh <93211764+Musta-eideh@users.noreply.github.com>
2023-01-31 11:08:46 -0300

    Primeira pasta de projeto, conversor de moedas

---
+++ ConversorDeMoedas/script.js
@@ -0,0 +1,7 @@
+let valueDollar = 30;
+let quotationInDollar = 5.32;
+
+let quotationValue = valueDollar*quotationInDollar;
+quotationValue = quotationValue.toFixed(2);
+
+alert('R$' + quotationValue);
